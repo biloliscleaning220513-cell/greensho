c9f186d2714ac26422bbe63b55dd1b6739777b56
nuriddin <mutalovnuriddin651@gmail.com>
2026-03-28 09:40:16 +0500

    update static 2

--- config/settings.py
+++ config/settings.py
@@ -149,16 +149,18 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+
 # settings.py
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Static fayllar uchun (CSS, JS, Images)
 STATIC_URL = '/static/'
+
 STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 # ... mavjud kodlar ...
 
 LOGIN_REDIRECT_URL = 'product_list'
